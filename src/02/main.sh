a07e179784fe48ea1c947db648ebfe2c8e1a72f3
virtName <virt>
2024-12-21 09:17:56 +0300

    n

--- src/02/main.sh
+++ src/02/main.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo "HOSTNAME =" $HOSTNAME
-echo "TIMEZONE =" `cat /etc/timezone `  " UTC "  ` date + "%:z ` 
+echo "TIMEZONE = `cat /etc/timezone `  UTC ` date +\"%-:::z\" `"
+echo "USER = " $USER
+echo "OS = " `cat /etc/issue`
+echo "DATE = ` date +\"%d %B %Y %T\"` "
