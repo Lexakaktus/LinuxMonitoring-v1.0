a4e151c3154a4299b668d424e12e0f598ab45cda
virtName <virt>
2024-12-22 13:40:02 +0300

    main"

--- src/02/main.sh
+++ src/02/main.sh
@@ -4,3 +4,6 @@ echo "TIMEZONE = `cat /etc/timezone `  UTC ` date +\"%-:::z\" `"
 echo "USER = " $USER
 echo "OS = " `cat /etc/issue`
 echo "DATE = ` date +\"%d %B %Y %T\"` "
+echo "UPTIME = `uptime -p` "
+echo " UPTIME_SEC =  `awk '{print $1}' /proc/uptime` "
+echo "IP =  ` ip -br -c r `| awk '{print $2}' "
