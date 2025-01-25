6bcfd83545cfdcbcf6be8ada484f70c5e0d9afe7
Ezreal Traci <ezrealtr@student.21-school.ru>
2025-01-25 17:17:42 +0300

    k

--- src/03/param.sh
+++ src/03/param.sh
@@ -8,17 +8,17 @@
 # if [ "$1" -eq "$b" ]
 case "$1" in
     1)
-        signs=\033[97m ;;
+        signs="\033[97m" ;;
     2)
-        signs=\033[91m ;;
+        signs="\033[91m" ;;
     3)
-        signs=\033[92m ;;
+        signs="\033[92m" ;;
     4)
-        signs=\033[94m ;;
+        signs="\033[94m" ;;
     5)
-        signs=\033[35m ;;
+        signs="\033[35m" ;;
     6)
-        signs=\033[30m ;;
+        signs="\033[30m" ;;
 
 esac
-echo " ${signs} hjkkbfs ${signs} \033[0m "
+echo " ${signs}hjkkbfs${signs} "
